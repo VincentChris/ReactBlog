3f6f89b38d146d9490914e49747c08f89422835b
VincentChris <vincentchris@126.com>
2020-10-04 00:31:53 +0800

    内容布局编写完毕

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "./index.module.scss";
 import React from "react";
 import Header from "../components/Header";
+import { Col, Row } from "antd";
 
 const Home: React.FC<null> = () => {
   return (
@@ -9,9 +10,17 @@ const Home: React.FC<null> = () => {
       <div className={styles.container}>
         <Head>
           <title>Home</title>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/images/favicon.ico" />
         </Head>
         <Header />
+        <Row justify="center" className={styles.main}>
+          <Col className={styles.left} xs={24} sm={24} md={16} lg={18} xl={14}>
+            left
+          </Col>
+          <Col className={styles.right} xs={0} sm={0} md={7} lg={5} xl={4}>
+            right
+          </Col>
+        </Row>
       </div>
     </>
   );
